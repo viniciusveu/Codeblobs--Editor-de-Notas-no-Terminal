299482b92f99b82a4653a0e5fb6a99d1a1e1c837
Vinícius Vedovotto <32825138+viniciusveu@users.noreply.github.com>
2018-06-25 02:54:23 -0300

    Add files via upload

---
+++ editor.c
@@ -0,0 +1,292 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <curses.h>
+
+typedef struct Texto_t{
+	struct Texto_t * ant;
+	char letra;
+	struct Texto_t * pro;
+} * Texto;
+
+typedef Texto Cursor;
+
+typedef struct Linha_l {
+	struct Texto_t * elem;
+	struct Linha_l * antL;
+	struct Linha_l * proxL;
+} * Linha;
+
+static char * * strings = NULL;
+static unsigned int n_strings = 0;
+
+Linha linha_novo(Linha ant, Texto texto) {
+	Linha linha = malloc(sizeof(struct Linha_l));
+	linha->antL = ant;
+	linha->elem = texto;
+	linha->proxL = NULL;
+	return linha;
+}
+
+Texto texto_novo(char caractere){
+	Texto lista = malloc(sizeof(struct Texto_t));
+	if(lista == NULL){
+		return NULL;
+	}
+	lista->ant = NULL;
+	lista->letra = caractere;
+	lista->pro = NULL;
+	return lista;
+}
+
+void texto_apagar(Texto texto){
+	for(;texto->ant != NULL;texto = texto->ant);
+	texto = texto->pro;
+	for(;texto->pro != NULL;texto = texto->pro){
+		free(texto->ant);
+	}
+	free(texto->ant);
+	free(texto);
+}
+
+char * texto_parastring(Texto texto,Cursor cursor){
+	if(texto == NULL){
+		return NULL;
+	}
+	while(texto->pro != NULL){
+		texto = texto->pro;
+	}
+	unsigned int tamanho;
+	unsigned int cursorpos = 0;
+	for(tamanho = 1;texto->ant != NULL;++tamanho,texto = texto->ant);
+	Texto i;
+	if(cursor != NULL){
+		for(i = texto;i != cursor;i = i->pro);
+		for(;i != NULL;++cursorpos){
+			i = i->pro;
+		}
+	}
+	tamanho += cursorpos + 1;
+	char * string = calloc(tamanho,sizeof(char));
+	unsigned int pos = 0;
+	for(i = texto;i != NULL;i = i->pro){
+		string[pos] = i->letra;
+		++pos;
+	}
+	if(cursor != NULL){
+		unsigned int j;
+		for(i = texto,j = 0;j < cursorpos;++j,i = i->pro){
+			string[pos] = '\b';
+			++pos;
+		}
+	}
+	string[pos] = '\0';
+	strings = realloc(strings,sizeof(char * *)*n_strings);
+	strings[n_strings] = string;
+	++n_strings;
+	return string;
+}
+
+Texto texto_abrir(char * const arquivo,Linha linha){
+	unsigned int i;
+	for(i = 0;arquivo[i] != '\0';++i){
+		if(arquivo[i] == '\n'){
+			arquivo[i] = '\0';
+			break;
+		}
+	}
+	FILE * arq = fopen(arquivo,"r");
+	Texto texto = malloc(sizeof(struct Texto_t));
+	linha = linha_novo(NULL, texto);
+	texto->ant = NULL;
+	texto->pro = NULL;
+	if(arq == NULL){
+		return NULL;
+	}
+	while(!feof(arq)){
+		texto->letra = fgetc(arq);
+		if(texto->letra == '\n') {
+			linha->proxL = linha_novo(linha, texto->pro);
+		}
+		texto->pro = malloc(sizeof(struct Texto_t));
+		texto->pro->ant = texto;
+		texto->pro->pro = NULL;
+		texto = texto->pro;	
+	}
+	texto = texto->ant;
+	free(texto->pro);//apaga o caractere alocado a mais
+	texto = texto->ant;
+	free(texto->pro);//apaga o '\n' do final do arquivo
+	texto = texto->ant;
+	free(texto->pro);//apaga o EOF de texto
+	texto->pro = NULL;
+	fclose(arq);
+	return texto;
+}
+
+void texto_salvar(Texto texto,char * nome_do_arquivo){
+	for(;texto->ant != NULL;texto = texto->ant);
+	unsigned int i;
+	for(i = 0;nome_do_arquivo[i] != '\0';++i){
+		if(nome_do_arquivo[i] == '\n'){
+			nome_do_arquivo[i] = '\0';
+			break;
+		}
+	}
+	FILE * arquivo = fopen(nome_do_arquivo,"w");
+	if(arquivo == NULL){
+		return;
+	}
+	for(;texto != NULL;texto = texto->pro){
+		fputc(texto->letra,arquivo);
+	}
+	fputc('\n',arquivo);
+	fclose(arquivo);
+}
+
+Cursor texto_inserirchar(Texto texto,Cursor cursor,char caractere, Linha linha){
+	if(texto == NULL){
+		return NULL;
+	}
+	if(cursor == NULL){
+		while(texto->pro != NULL){
+			texto = texto->pro;
+		}
+	}
+	Texto novo = malloc(sizeof(struct Texto_t));
+	novo->letra = caractere;
+	if(cursor != NULL){
+		novo->ant = cursor->ant;
+		novo->pro = cursor;
+		if(cursor->ant != NULL){
+			cursor->ant->pro = novo;
+		}
+		cursor->ant = novo;
+	}else{
+		novo->ant = texto;
+		novo->pro = NULL;
+		texto->pro = novo;
+	}
+	if(caractere == '\n') {
+		linha->proxL = linha_novo(linha, cursor);
+	}
+	return novo;
+}
+
+void texto_deletarchar(Texto * texto,Cursor * cursor){
+	if((*cursor) == NULL){
+		return;
+	}
+	if(((*cursor)->ant == NULL)&&((*cursor)->pro == NULL)){
+		return;
+	}
+	if((*cursor)->pro != NULL){
+		Texto aux = *cursor;
+		if(texto == cursor){
+			*texto = (*texto)->pro;
+		}
+		*cursor = (*cursor)->pro;
+		if((*cursor)->ant->ant != NULL){
+			(*cursor)->ant->ant->pro = *cursor;
+		}
+		(*cursor)->ant = (*cursor)->ant->ant;
+		free(aux);
+		return;
+	}else{
+		if((*cursor)->ant != NULL){
+			if((*cursor) == (*texto)){
+				(*texto) = (*texto)->ant;
+			}
+			(*cursor) = (*cursor)->ant;
+			free((*cursor)->pro);
+			(*cursor)->pro = NULL;
+			(*cursor) = NULL;
+			return;
+		}else{
+			(*cursor)->letra = ' ';
+			return;
+		}
+	}
+}
+
+void texto_bdeletarchar(Texto * texto,Cursor * cursor){
+	//falta aqui
+	return;
+}
+
+void texto_movercursor(Texto texto,Cursor * cursor,long int num){
+	if(texto == NULL){
+		return;
+	}
+	if(num == 0){
+		return;
+	}
+	if(num > 0){
+		if((*cursor) == NULL){
+			return;
+		}else{
+			while((num)&&((*cursor) != NULL)){
+				*cursor = (*cursor)->pro;
+				--num;
+			}
+			return;
+		}
+	}else{
+		if((*cursor) == NULL){
+			(*cursor) = texto;
+			while((*cursor)->pro != NULL){
+				*cursor = (*cursor)->pro;
+			}
+			++num;
+		}
+		if((*cursor)->ant == NULL){
+			return;
+		}
+		while((num)&&((*cursor)->ant != NULL)){
+			*cursor = (*cursor)->ant;
+			++num;
+		}
+		return;
+	}
+}
+
+void texto_limpar(){
+	unsigned int i;
+	for(i = 0;i < n_strings;++i){
+		free(strings[i]);
+	}
+	free(strings);
+	strings = NULL;
+	n_strings = 0;
+}
+
+/*int procurar(char * palavra,char * texto){
+	 
+	 unsigned int i;
+	 for(i = 0;palavra[i] != '\0';++i){
+	   if(palavra[i] == '\n'){
+	     palavra[i] = '\0';
+	     break;
+	   }
+	 }
+	 char * delims = " ";
+     char * result = NULL;
+     result = strtok( texto, delims );
+     
+     while( result != NULL ) {
+         if(strcmp(palavra, result) == 0) {
+            attron(A_BOLD);
+            printw("%s ", palavra);
+            attroff(A_BOLD);
+            refresh();
+            result = strtok( NULL, delims );
+         }else
+         {
+            printw("%s ", result);
+            refresh();
+            result = strtok( NULL, delims );
+         }
+     }
+     getch();
+     printw ("\n");
+}*/
+
---
+++ main.c
@@ -0,0 +1,118 @@
+#include <stdlib.h>
+#include <curses.h>
+#include <string.h>
+#include "editor.h"
+#include "editor.c"
+
+Linha linha = NULL;
+Texto fluxo = NULL;
+Cursor pfluxo = NULL;
+
+
+void sair(char* nome_arquivo){
+  endwin();
+  texto_salvar(fluxo,nome_arquivo);
+  texto_apagar(fluxo);
+  printf("\nTexto salvo! :D\n");
+}
+
+int main(int argc, char *argv[]){
+ 
+  char op; char nome_arquivo[200];
+ 
+  printf("Deseja criar novo arquivo de texto ou abrir existente? [a,n]: ");
+  scanf("%c", &op);
+  if(op == 'a') {
+    printf("Qual o nome do arquivo? [sem espaços]\n");
+    scanf("%s", nome_arquivo);
+  } else if(op == 'n') {
+    printf("Qual será o nome do seu arquivo? [sem espaços]\n");
+	  scanf("%s", nome_arquivo); 
+    FILE* f;	
+	  if( (f=fopen(nome_arquivo, "w+r")) == NULL) {
+      printf("ERRO na criação do arquivo %s!!! \n", nome_arquivo);
+      exit(1);
+    }
+	  fputs("Escreva aqui  ", f);
+    fclose(f);
+ 	} else printf("Entrada inválida!\n");
+  
+  fluxo = texto_abrir(nome_arquivo, linha);
+  pfluxo = fluxo;
+
+  initscr();
+  keypad(stdscr,TRUE);
+  erase();
+  printw("%s",texto_parastring(fluxo,pfluxo));
+  refresh();
+ 
+  int ch;
+ 
+  while(1){
+    switch(ch = getch()){
+   
+      case KEY_BACKSPACE:
+        texto_movercursor(fluxo,&pfluxo,-1);
+        texto_deletarchar(&fluxo,&pfluxo);	
+        break;
+
+      case KEY_LEFT:
+        texto_movercursor(fluxo,&pfluxo,-1);
+        break;
+   
+      case KEY_RIGHT:
+        texto_movercursor(fluxo,&pfluxo,1);
+        break;
+   
+      case KEY_UP:
+        texto_movercursor(fluxo,&pfluxo,-64);
+        break;
+   
+      case KEY_DOWN:
+        texto_movercursor(fluxo,&pfluxo,64);
+        break;
+   
+      case KEY_HOME:
+        texto_movercursor(fluxo,&pfluxo,-100000);
+        break;
+   
+      case KEY_END:
+        texto_movercursor(fluxo,&pfluxo,32);
+        break;
+   
+      case KEY_DC:
+        texto_deletarchar(&fluxo,&pfluxo);
+        break;
+   
+      /*case KEY_IC:
+        erase();
+   	    printw ("Digite a palavra a ser procurada:\n");
+   	    scanw("%19s",palavra);
+   	    procurar(palavra,texto_parastring(fluxo,pfluxo));
+   	    break;*/
+   	
+      case '\n':
+        printw("\n");
+        //mostre o menu
+        sair(nome_arquivo);
+        exit(EXIT_SUCCESS);
+      break;
+   
+      default:
+        if(ch < 256){
+          texto_inserirchar(fluxo,pfluxo,ch,linha);
+        }
+    
+        else{
+          printw("\a");
+        }
+    }
+  
+    erase();
+    printw("%s",texto_parastring(fluxo,pfluxo));
+    refresh();
+    texto_limpar();
+  }
+  sair(nome_arquivo);
+  exit(EXIT_SUCCESS);
+}
